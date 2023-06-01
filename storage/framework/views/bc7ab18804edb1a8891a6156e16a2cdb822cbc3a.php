1fc80fd06f360b8c3b28e06dc9f7d9406f9be081
Michael Mak <45162785+michaelmak0904@users.noreply.github.com>
2023-06-01 16:35:03 +0800

    Init

---
+++ storage/framework/views/bc7ab18804edb1a8891a6156e16a2cdb822cbc3a.php
@@ -0,0 +1,4 @@
+<form action="/session" method="POST">
+    <input type="hidden" name="_token" value="<?php echo e(csrf_token()); ?>">
+    <button type="submit" id="checkout-live-button">Checkout</button>
+</form><?php /**PATH C:\Users\michaelmak\Documents\stripe-laravel\resources\views/index.blade.php ENDPATH**/ ?>
